a186aa977015e1b3c2378c0db3e7a738a1554f92
Nusrat Shakarov <nusrat558@gmail.com>
2018-01-06 13:27:12 +0300

    debug

--- src/main/java/com/shnus/VoteBotApplication.java
+++ src/main/java/com/shnus/VoteBotApplication.java
@@ -1,16 +1,12 @@
 package com.shnus;
 
-import com.shnus.VoteBot.VotePollingBot;
 import com.shnus.VoteBot.VotePollingBotRegister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 
 @SpringBootApplication
 public class VoteBotApplication implements ApplicationRunner {
